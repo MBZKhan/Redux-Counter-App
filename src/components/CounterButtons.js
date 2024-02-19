8717928c716aa6ce8c7e2df8e324d4a0222ecc86
Muhammad Bilal Zahid Khan <muhammadbilalzahidkhan@gmail.com>
2024-02-19 18:53:48 +0500

    updated

--- src/components/CounterButtons.js
+++ src/components/CounterButtons.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { View, Button } from 'react-native';
-import { useDispatch } from 'react-redux';
-import { increment, decrement } from '../store/reducers'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement } from '../store/reducers';
 
 const CounterButtons = () => {
   const dispatch = useDispatch();
+  const count = useSelector(state => state.counter.count);
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      dispatch(decrement());
+    }
+  };
 
   return (
     <View>
-      <Button title="Increment" onPress={() => dispatch(increment())} />
-      <Button title="Decrement" onPress={() => dispatch(decrement())} />
+      <Button title="Increment" onPress={handleIncrement} />
+      {count > 0 && <Button title="Decrement" onPress={handleDecrement} />}
     </View>
   );
 };
